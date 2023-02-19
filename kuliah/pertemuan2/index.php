e4bf5551b419d56631ad2221eaab40128d3d4133
Fauzan2617 <mfauzandwiputera10@gmail.com>
2023-02-19 10:45:16 +0700

    Tugas 1

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "Fauzan";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 2</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo 100 + 200;
+        echo "$nama";
+        echo "hari jum'at kita \"ujian"
+        ?>
+    </h1>
+
+    <p><?php echo "pemrograman web"; ?></p>
+</body>
+
+</html>
