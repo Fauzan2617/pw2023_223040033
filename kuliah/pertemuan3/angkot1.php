9f8f0e78df058ae32a9e49a41ca4630b1499fd16
Fauzan2617 <mfauzandwiputera10@gmail.com>
2023-02-28 11:15:21 +0700

    Tugas 3 Finish

---
+++ kuliah/pertemuan3/angkot1.php
@@ -0,0 +1,9 @@
+
+<?php
+$angkotawal = 1;
+while ($angkotawal <= 10) {
+    echo "Angkot no $angkotawal beroprasi dengan baik <br>";
+    $angkotawal++;
+}
+echo "anjing bab"
+?>
---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,35 @@
+<?php
+// echo "Mulai</br>";
+// echo "Hello World </br>";
+// echo "Hello World </br>";
+// echo "Hello World </br>";
+// echo "Hello World </br>";
+// echo "Hello World </br>";
+// echo "Hello World </br>";
+
+// Pengulangan While
+// 1.inisialisation / nilai awal
+// 2. Kondisi Terminasi / kapan pengulangan berhenti
+// 3. Increment / Decrement 
+
+// while (true) {
+//     echo "hello world";
+// }
+
+
+
+// $nilai_awal = 5;
+// while ($nilai_awal >= 1) {
+//     echo "Hello World $nilai_awal x</br>";
+//     $nilai_awal--;
+// }
+// $nilai_awal = 1;
+// while ($nilai_awal <= 5) {
+//     echo "Hello World $nilai_awal x</br>";
+//     $nilai_awal++;
+// }
+
+// for
+for ($nilai_awal = 1; $nilai_awal <= 10; $nilai_awal++) {
+    echo "Hello World $nilai_awal x <br>";
+}
